8d645de46f26c2881406450466132eef36ab097e
Khushali Dudhabade <khushalidudhabade27@gmail.com>
2024-07-17 13:45:41 +0530

    keep changes

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,7 @@ const App = () => {
   return (
     <div>
         
-        <h3>Enter Your Name :</h3>
+        <p>Enter Your Name :</p>
         <input type="text" value={input} onChange={(e) => setInput(e.target.value)}/>
         {input && <p>{`Hello ${input} !`}</p>}
     </div>
